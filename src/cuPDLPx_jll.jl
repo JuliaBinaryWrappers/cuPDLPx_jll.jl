5f280515629555f8de8d27c08dc91e8ce5c9fcaa
jlbuild <juliabuildbot@gmail.com>
2025-12-20 14:50:58 +0000

    cuPDLPx_jll build 0.1.5+0

--- src/cuPDLPx_jll.jl
+++ src/cuPDLPx_jll.jl
@@ -6,5 +6,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("cuPDLPx")
-JLLWrappers.@generate_main_file("cuPDLPx", UUID("bca5daad-f4d3-5101-ae12-8b63679c982c"))
+JLLWrappers.@generate_main_file("cuPDLPx", Base.UUID("bca5daad-f4d3-5101-ae12-8b63679c982c"))
 end  # module cuPDLPx_jll
